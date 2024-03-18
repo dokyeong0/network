e88f6110b683380da145360f6427d697328f2222
Do Kyeong Kim <dkdwkim@naver.com>
2024-03-19 03:14:54 +0900

    create doke.py

--- doke/doke.py
+++ doke/doke.py
@@ -1,10 +1,6 @@
-score = int(input("정수 입력:"))
-if score >= 90:
-    print("성적 : A")
-    print("장학금 수여")
+year = int(input("Type a year:"))
+if (year%4 == 0 and year%100 != 0) or (year%400) ==0 :
+    print(year,"is leap year")
 
-age_0 = 22
-age_1 = 18
-age = int(input('your age? '))
-if age < age_0 and age > age_1:
-    print("당신의 나이는 18~22세 사이입니다")
+else:
+    print(year, "is not a leap year")
