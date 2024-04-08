b599f05590ab90828a59f3dd2e8235c30bf2e9c8
Do Kyeong Kim <dkdwkim@naver.com>
2024-04-09 05:09:04 +0900

    create 0409.py

--- 0409.py
+++ 0409.py
@@ -1,18 +1,4 @@
-def gcd(a,b):
-    larger = 0
-    smaller = 100000
-    if (a>b):
-        larger = a
-        smaller = b
-    else:
-        larger = b
-        smaller = a
-    while smaller > 0:
-        rem = larger % smaller 
-        larger = smaller
-        smaller = rem
-    return larger
-
-if __name__ == "__main__":
-    res = gcd(256,240)
-    print(res)
+word = 'Buffalo'
+index = word.find('a') + 1
+print(word[:index])
+print(word[index:])
